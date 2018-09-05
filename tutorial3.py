f02ba4529507315b3d6678a0fd907445c5a53fff
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-05 16:15:36 -0400

    Updated from Brython Server: 9/5/2018 4:15:34 PM

--- tutorial3.py
+++ tutorial3.py
@@ -11,4 +11,17 @@ noline = LineStyle(0, black)
 bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
 bg = Sprite(bg_asset, (0,0))
 
+# A ball! This is already in the ggame-tutorials repository
+ball_asset = ImageAsset("images/orb-150545_640.png")
+ball = Sprite(ball_asset, (0, -100))
+# Original image is too big. Scale it to 1/10 its original size
+ball.scale = 0.1
+# custom attributes
+ball.direction = 1
+ball.go = True
+
+# reverse - change the ball direction
+def reverse(b):
+    b.direction *= -1
+
 myapp.run()
