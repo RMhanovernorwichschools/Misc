49ef350dd0655f514929124ea926bc8c7948dba3
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-05 16:18:18 -0400

    Updated from Brython Server: 9/5/2018 4:18:16 PM

--- tutorial3.py
+++ tutorial3.py
@@ -13,7 +13,7 @@ bg = Sprite(bg_asset, (0,0))
 
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
-ball = Sprite(ball_asset, (0, -100))
+ball = Sprite(ball_asset, (0, 0))
 # Original image is too big. Scale it to 1/10 its original size
 ball.scale = 0.1
 # custom attributes
@@ -32,4 +32,4 @@ def step():
             ball.x -= ball.direction
             reverse(ball)
 
-myapp.run()
+myapp.run(step)
