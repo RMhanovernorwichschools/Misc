c2c40cb003c1620ff8c5b9d13210e8b07e426573
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-05 16:25:36 -0400

    Updated from Brython Server: 9/5/2018 4:25:34 PM

--- tutorial3.py
+++ tutorial3.py
@@ -1,10 +1,10 @@
-from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset, CircleAsset
 from ggame import LineStyle, Color, Sprite, Sound
 
 myapp = App()
 
 # define colors and line style
-green = Color(0x00ff00, 1)
+green = Color(0x00ff00, 0.1)
 black = Color(0, 1)
 noline = LineStyle(0, black)
 # a rectangle asset and sprite to use as background
@@ -12,10 +12,9 @@ bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
 bg = Sprite(bg_asset, (0,0))
 
 # A ball! This is already in the ggame-tutorials repository
-ball_asset = ImageAsset("images/orb-150545_640.png")
+ball_asset = CircleAsset(20)
 ball = Sprite(ball_asset, (0, 0))
 # Original image is too big. Scale it to 1/10 its original size
-ball.scale = 0.1
 # custom attributes
 ball.direction = 1
 ball.go = True
@@ -31,5 +30,24 @@ def step():
         if ball.x + ball.width > myapp.width or ball.x < 0:
             ball.x -= ball.direction
             reverse(ball)
+            print('Bounce!')
+            
+# Handle the space key
+def spaceKey(event):
+    ball.go = not ball.go
+
+# Handle the "reverse" key
+def reverseKey(event):
+    reverse(ball)
+
+# Handle the mouse click
+def mouseClick(event):
+    ball.x = event.x
+    ball.y = event.y
+    
+# Set up event handlers for the app
+myapp.listenKeyEvent('keydown', 'space', spaceKey)
+myapp.listenKeyEvent('keydown', 'r', reverseKey)
+myapp.listenMouseEvent('click', mouseClick)
 
 myapp.run(step)
